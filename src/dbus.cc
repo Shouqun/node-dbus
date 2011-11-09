faefdbfa5a3e59014ffbdf6bfd4e75b98b4719c8
Shouqun Liu <liushouqun@gmail.com>
2011-11-09 22:25:18 +0800

    Merge the glib main loop into libev main loop of node, which is essential to emit signal from the glib source into node context. TODO:   in non-interactive mode, the mainloop blocks at "check_cb" during "g_main_context_check"   this is because "prepare_cb" is not called. need to clarify this issue

--- src/dbus.cc
+++ src/dbus.cc
@@ -1,5 +1,6 @@
 #include <v8.h>
 #include <node.h>
+#include <glib.h>
 #include <dbus/dbus.h>
 #include <dbus/dbus-glib.h>
 #include <dbus/dbus-glib-lowlevel.h>
@@ -992,6 +993,113 @@ Handle<Value> GetInterface(const Arguments& args) {
   return scope.Close(interface_object); 
 }
 
+/// Add glib evene loop to libev
+struct econtext {
+  GPollFD *pfd;
+  ev_io *iow;
+  int nfd, afd;
+  gint maxpri;
+
+  ev_prepare pw;
+  ev_check cw;
+  ev_timer tw;
+
+  GMainContext *gc;
+};
+
+static void timer_cb (EV_P_ ev_timer *w, int revents) {
+  /* nop */
+}
+
+static void io_cb (EV_P_ ev_io *w, int revents) {
+  /* nop */
+}
+
+static void prepare_cb (EV_P_ ev_prepare *w, int revents) {
+  struct econtext *ctx = (struct econtext *)(((char *)w) - offsetof (struct econtext, pw));
+  gint timeout;
+  int i;
+
+  g_main_context_dispatch (ctx->gc);
+
+  g_main_context_prepare (ctx->gc, &ctx->maxpri);
+
+  while (ctx->afd < (ctx->nfd = g_main_context_query  (
+          ctx->gc,
+          ctx->maxpri,
+          &timeout,
+          ctx->pfd,
+          ctx->afd))
+      )
+  {
+    free (ctx->pfd);
+    free (ctx->iow);
+
+    ctx->afd = 1;
+    while (ctx->afd < ctx->nfd)
+      ctx->afd <<= 1;
+
+    ctx->pfd = (GPollFD*) malloc (ctx->afd * sizeof (GPollFD));
+    ctx->iow = (ev_io*) malloc (ctx->afd * sizeof (ev_io));
+  }
+
+  for (i = 0; i < ctx->nfd; ++i)
+  {
+    GPollFD *pfd = ctx->pfd + i;
+    ev_io *iow = ctx->iow + i;
+
+    pfd->revents = 0;
+
+    ev_io_init (
+        iow,
+        io_cb,
+        pfd->fd,
+        (pfd->events & G_IO_IN ? EV_READ : 0)
+        | (pfd->events & G_IO_OUT ? EV_WRITE : 0)
+        );
+    iow->data = (void *)pfd;
+    ev_set_priority (iow, EV_MINPRI);
+    ev_io_start (EV_A iow);
+  }
+
+  if (timeout >= 0)
+  {
+    ev_timer_set (&ctx->tw, timeout * 1e-3, 0.);
+    ev_timer_start (EV_A &ctx->tw);
+  }
+}
+
+static void check_cb (EV_P_ ev_check *w, int revents) {
+  struct econtext *ctx = (struct econtext *)(((char *)w) - offsetof (struct econtext, cw));
+  int i;
+
+  for (i = 0; i < ctx->nfd; ++i)
+  {
+    ev_io *iow = ctx->iow + i;
+
+    if (ev_is_pending (iow))
+    {
+      GPollFD *pfd = ctx->pfd + i;
+      int revents = ev_clear_pending (EV_A iow);
+
+      pfd->revents |= pfd->events &
+        ((revents & EV_READ ? G_IO_IN : 0)
+         | (revents & EV_WRITE ? G_IO_OUT : 0));
+    }
+
+    ev_io_stop (EV_A iow);
+  }
+
+  if (ev_is_active (&ctx->tw))
+    ev_timer_stop (EV_A &ctx->tw);
+
+  //oops, event loop  block here, the "prepare_cb" is never called and so blocks here
+  // in the interactive node, this never blocks.  but launch with "node test_method.js"
+  // blocks here on "read" inside g_main_context_check
+  g_main_context_check (ctx->gc, ctx->maxpri, ctx->pfd, ctx->nfd);
+}
+
+static struct econtext default_context;
 
 extern "C" void
 init (Handle<Object> target)
@@ -1008,4 +1116,27 @@ init (Handle<Object> target)
 
 
   g_type_init();
+
+  //add glib event loop to libev main loop in node
+  GMainContext *gc     = g_main_context_default();
+  struct econtext *ctx = &default_context;
+
+  ctx->gc  = g_main_context_ref(gc);
+  ctx->nfd = 0;
+  ctx->afd = 0;
+  ctx->iow = 0;
+  ctx->pfd = 0;
+
+  ev_prepare_init (&ctx->pw, prepare_cb);
+  ev_set_priority (&ctx->pw, EV_MINPRI);
+  ev_prepare_start (EV_DEFAULT_UC_ &ctx->pw);
+  ev_unref(EV_DEFAULT_UC_);
+
+  ev_check_init (&ctx->cw, check_cb);
+  ev_set_priority (&ctx->cw, EV_MAXPRI);
+  ev_check_start (EV_DEFAULT_UC_ &ctx->cw);
+  ev_unref(EV_DEFAULT_UC_);
+
+  ev_init (&ctx->tw, timer_cb);
+  ev_set_priority (&ctx->tw, EV_MINPRI);
 }
