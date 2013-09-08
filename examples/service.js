c4373fbf3f816112ea05501657e87294042afded
Fred Chien <fred@mandice.com>
2013-09-08 20:37:01 +0800

    modified service example

--- examples/service.js
+++ examples/service.js
@@ -9,7 +9,7 @@ var obj = service.createObject('/nodejs/dbus/ExampleService');
 // Create interface
 var iface1 = obj.createInterface('nodejs.dbus.ExampleService.Interface1');
 
-iface1.addMethod('Hello', null, function(callback) {
+iface1.addMethod('Hello', { out: String }, function(callback) {
 	callback('Hello There!');
 });
 
@@ -18,7 +18,7 @@ iface1.update();
 // Create second interface
 var iface2 = obj.createInterface('nodejs.dbus.ExampleService.Interface2');
 
-iface2.addMethod('Hello', null, function(callback) {
+iface2.addMethod('Hello', { out: String }, function(callback) {
 	callback('Hello There!');
 });
 
