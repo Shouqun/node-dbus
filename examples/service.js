f4aab937ab84a98927e1db58c67b68c2e3c3af05
Fred Chien <fred@mandice.com>
2013-09-08 20:33:47 +0800

    add method to update introspection of interface

--- examples/service.js
+++ examples/service.js
@@ -5,9 +5,21 @@ var dbus = new DBus();
 // Create a new service, object and interface
 var service = dbus.registerService('session', 'nodejs.dbus.ExampleService');
 var obj = service.createObject('/nodejs/dbus/ExampleService');
-var iface = obj.createInterface('nodejs.dbus.ExampleService.Interface1');
 
-// Create methods
-iface.addMethod('Hello', null, function(callback) {
+// Create interface
+var iface1 = obj.createInterface('nodejs.dbus.ExampleService.Interface1');
+
+iface1.addMethod('Hello', null, function(callback) {
 	callback('Hello There!');
 });
+
+iface1.update();
+
+// Create second interface
+var iface2 = obj.createInterface('nodejs.dbus.ExampleService.Interface2');
+
+iface2.addMethod('Hello', null, function(callback) {
+	callback('Hello There!');
+});
+
+iface2.update();
