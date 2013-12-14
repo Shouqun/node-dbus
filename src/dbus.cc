380fd61084688a49b07b400d312ad8bb57a31d0a
Fred Chien <cfsghost@gmail.com>
2013-12-14 11:34:58 +0800

    set error argument if service returned error type message

--- src/dbus.cc
+++ src/dbus.cc
@@ -40,6 +40,8 @@ namespace NodeDBus {
 		if (dbus_error_is_set(&error)) {
 			err = Exception::Error(String::New(error.message));
 			dbus_error_free(&error);
+		} else if (dbus_message_get_type(message) == DBUS_MESSAGE_TYPE_ERROR) {
+			err = Exception::Error(String::New(dbus_message_get_error_name(message)));
 		}
 
 		// Decode message for arguments
