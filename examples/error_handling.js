f6d737049a8fb1f991863533bb3ad98f9f6d0b13
Bryan Burgers <bryan@burgers.io>
2017-02-08 10:27:02 -0600

    Use node conventions for asynchronous calls
    Change how methods are called on an interface from
    
        iface.someMethod.timeout = 1000;
        iface.someMethod.error = function(err) {
        };
        iface.someMethod.finish = function(result) {
        };
        iface.someMethod(arg1, arg2);
    
    to
    
        iface.someMethod(arg1, arg2, {timeout:1000}, function(err, result) {
        });
    
    or (if the timeout is not needed, the options object can be dropped)
    
        iface.someMethod(arg1, arg2, function(err, result) {
        });
    
    to be more in-line with node's error-first callback conventions around
    asynchronous calls.
    
    Fixes #86.

--- examples/error_handling.js
+++ examples/error_handling.js
@@ -6,13 +6,12 @@ var bus = dbus.getBus('session');
 
 bus.getInterface('nodejs.dbus.ExampleService', '/nodejs/dbus/ExampleService', 'nodejs.dbus.ExampleService.Interface1', function(err, iface) {
 
-	iface.MakeError['timeout'] = 1000;
-	iface.MakeError['error'] = function(err) {
-		console.log(err);
-	};
-	iface.MakeError['finish'] = function(result) {
-		console.log(result);
-	};
-	iface.MakeError();
+	iface.MakeError({ timeout: 1000 }, function(err, result) {
+		if (err) {
+			console.log(err);
+			return;
+		}
 
+		console.log(result);
+	});
 });
